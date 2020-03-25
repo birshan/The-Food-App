2fce1ee47fa1b49732f017b8c8af036fad81167c
Shanil De Silva <shanil.desilva@hotmail.com>
2020-03-25 17:06:33 +0530

    simple api to upload an image

--- DataScience/SDGP/FlaskApp/app.py
+++ DataScience/SDGP/FlaskApp/app.py
@@ -5,21 +5,22 @@ import cv2
 import numpy as np
 import os
 
+
 UPLOAD_FOLDER = 'D:\IIT\Group Project\The-Food-App\DataScience\SDGP\FlaskApp\images'
 ALLOWED_EXTENTIONS = {'jpeg', 'jpg', 'png'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+directory = 'D:\IIT\Group Project\The-Food-App\DataScience\SDGP\FlaskApp\images'
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENTIONS
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 
 @app.route('/upload_image/', methods=['POST', 'GET'])
@@ -35,10 +36,12 @@ def upload_image():
             return redirect('/')
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            #to save in the os, change to instead transform and show
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_image',
-                                    filename=filename))
+            # to save in the os, change to instead transform and show
+            img = cv2.imdecode(np.frombuffer(request.files['file'].read(), np.uint8), cv2.IMREAD_UNCHANGED)
+            half = cv2.resize(img, (0, 0), fx=0.1, fy=0.1)
+            os.chdir(directory)
+            cv2.imwrite(filename, half)
+            return render_template('index.html', filename=filename, file=file)
     return render_template('index.html')
 
 
