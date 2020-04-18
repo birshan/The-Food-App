d8914162c59eb6ef1b6d3dd28f464fbf4f1c4465
Birshan Ramzy Caraccio <47294281+birshan@users.noreply.github.com>
2020-04-18 18:57:12 +0530

    foodpojo
    small change to make sure it fits the names in the excel file

--- BackEnd/src/main/java/com/foodapp/backend/pojo/Food.java
+++ BackEnd/src/main/java/com/foodapp/backend/pojo/Food.java
@@ -118,7 +118,7 @@ public class Food {
     private String servingDescription8;
     private double servingWeight9_g;
     private String servingDescription9;
-    private double calorieWeight_g;
+    private double calorieWeight_200g;
 
 
     public Integer getId() {
@@ -1018,10 +1018,10 @@ public class Food {
     }
 
     public double getCalorieWeight_g() {
-        return calorieWeight_g;
+        return calorieWeight_200g;
     }
 
     public void setCalorieWeight_g(double calorieWeight_g) {
-        this.calorieWeight_g = calorieWeight_g;
+        this.calorieWeight_200g = calorieWeight_g;
     }
 }
