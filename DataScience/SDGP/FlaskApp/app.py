ff6fd4f981dd5c0130946a5920665fb836fc512d
Shanil De Silva <shanil.desilva@hotmail.com>
2020-04-26 18:43:33 +0530

    image can be sent to server as base64

--- DataScience/SDGP/FlaskApp/app.py
+++ DataScience/SDGP/FlaskApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, jsonify
 from werkzeug.utils import secure_filename
 from datetime import datetime
 import cv2
@@ -26,24 +26,52 @@ def index():
 @app.route('/upload_image/', methods=['POST', 'GET'])
 def upload_image():
     if request.method == 'POST':
+        print(request)
         # checking for file
         if 'file' not in request.files:
             print('no file')
-            return redirect('/')
+            raise InvalidUsage("No file uploaded", status_code=409)
         file = request.files['file']
         if file.filename == '':
             print('no selected file')
-            return redirect('/')
+            raise InvalidUsage("File not selected", status_code=409)
         if file and allowed_file(file.filename):
+
             filename = secure_filename(file.filename)
             # to save in the os, change to instead transform and show
             img = cv2.imdecode(np.frombuffer(request.files['file'].read(), np.uint8), cv2.IMREAD_UNCHANGED)
             half = cv2.resize(img, (0, 0), fx=0.1, fy=0.1)
             os.chdir(directory)
             cv2.imwrite(filename, half)
-            return render_template('index.html', filename=filename, file=file)
+            print('file uploaded')
+            return ('file saved')
+        else:
+            print('some error occurred')
     return render_template('index.html')
+
+class InvalidUsage(Exception):
+    status_code: 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
+
