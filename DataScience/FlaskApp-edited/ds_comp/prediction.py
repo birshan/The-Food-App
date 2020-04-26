ba89dd5ec9fe34222548d229c7edc6d50edf1c6c
a-avishka <a.avishka99@gmail.com>
2020-04-26 10:44:55 +0530

    Added model

--- DataScience/FlaskApp-edited/ds_comp/prediction.py
+++ DataScience/FlaskApp-edited/ds_comp/prediction.py
@@ -19,37 +19,33 @@ class Prediction:
             "bread",
             "rice",
             "roti",
-            "noodles"
+            "noodles",
+            "kottu",
+            "pizza"
         ]
 
+        # builds the model using the saved structure and weights
         f = Path("ds_comp/model_structure.json")
         model_structure = f.read_text()
-
         model = model_from_json(model_structure)
-
         model.load_weights("ds_comp/model_weights.h5")
 
+        # converts the input image to a normalised numpy array
         img = image.load_img(self.file, target_size=(224, 224))
-
         image_array = image.img_to_array(img)
-
         images = np.expand_dims(image_array, axis=0)
-
         images = vgg16.preprocess_input(images)
-
+        # extracts the features of the new image
         feature_extraction_model = vgg16.VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
         features = feature_extraction_model.predict(images)
 
+        # Using the extracted features the model predicts what the image is
         results = model.predict(features)
 
-        print(results[0])
-
+        # prints the class label of the prediction using results produced
         single_result = results[0]
-
         most_likely_class_index = int(np.argmax(single_result))
-        class_likelihood = single_result[most_likely_class_index]
-
         class_label = class_labels[most_likely_class_index]
 
-        print("This is image is a {} - Likelihood: {:2f}".format(class_label, class_likelihood))
+        print("{}".format(class_label))
         return "{}".format(class_label)
